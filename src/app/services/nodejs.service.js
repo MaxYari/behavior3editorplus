b65646b62c9c41ebe0edb143c98c1eb2c047180b
Max Yari <maksyarik@gmail.com>
2024-06-17 16:57:40 +0200

    Reverting dialog function changes

--- src/app/services/nodejs.service.js
+++ src/app/services/nodejs.service.js
@@ -7,11 +7,12 @@ nodejsService.$inject = ['$window'];
 function nodejsService($window) {
   var ok = !!$window.require;
   var remote = (ok ? $window.require('electron').remote : null);
+
   var service = {
     ok: ok,
     fs: (ok ? $window.require('fs') : null),
     path: (ok ? $window.require('path') : null),
-    dialog: (ok ? $window.require('electron').dialog : null),
+    dialog: (ok ? remote.dialog : null),
   };
   return service;
 
